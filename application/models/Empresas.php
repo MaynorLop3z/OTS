ce3cdfe8488d7f4ca6103adc6d5d2e3fe37bcc02
MaynorLop3z <lj09002@ues.edu.sv>
2016-04-27 17:03:39 -0600

    backup de la base de datos general

---
+++ application/models/Empresas.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+class Participantes extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    
+    public function getEmpresas() {
+        $this->db->select('IdEmpresa, '
+                . 'Nombre, '
+                . 'BDName');
+        $this->db->from('Empresas');
+        $consulta = $this->db->get();
+        $resultado = $consulta->result();
+        return $resultado;
+    }
+}
