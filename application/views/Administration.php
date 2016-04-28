71309e6e7a7cf365f37d20acb93d1b052d921384
MaynorLop3z <lj09002@ues.edu.sv>
2016-04-28 17:05:15 -0600

    Agregado el modulo de administracion

---
+++ application/views/Administration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php $this->load->helper('url'); ?>
+        <meta charset="UTF-8">
+        <title>Login</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap -->          
+        <link href="../appearance/css/flatly-bootstrap.min.css" rel="stylesheet">
+        <script src="../appearance/js/jquery-2.2.3.min.js"></script>
+        <script src="../appearance/js/bootstrap.min.js"></script>
+        <link href="../appearance/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10"></div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
