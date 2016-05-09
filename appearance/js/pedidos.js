f5361008d50a5769b96645c2ef64c25645973af6
MaynorLop3z <lj09002@ues.edu.sv>
2016-05-08 23:02:08 -0600

    cambios en interfaz de pedidos

---
+++ appearance/js/pedidos.js
@@ -0,0 +1,10 @@
+var codigo;
+function showPaymentCash(fila){
+    codigo = fila.id;
+    $("#FormaDePago").modal('toggle');
+}
+function showPaymentOnline(fila){
+    codigo = fila.id;
+    $("#FormaDePago").modal('toggle');
+}
+
--- application/views/Orders.php
+++ application/views/Orders.php
@@ -15,20 +15,26 @@ and open the template in the editor.
         <link href="../appearance/css/paper-bootstrap.min.css" rel="stylesheet">
         <script src="../appearance/js/jquery-2.2.3.min.js"></script>
         <script src="../appearance/js/bootstrap.min.js"></script>
+        <script src="../appearance/js/pedidos.js"></script>
         <link href="../appearance/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     </head>
-    <body style="padding-top: 100px;">
+    <body style="padding-top: 110px;">
         <div class="container-fluid">
             <div class="row">
                 <nav class="navbar navbar-default navbar-fixed-top">
                     <div class="container-fluid">
                         <img src="../appearance/images/image3344.png" class="navbar-form navbar-left img-responsive" alt="Buffalo Wings"/>
-                        <p class="navbar-text navbar-right"><span class="fa-stack fa-lg">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-flag fa-stack-1x fa-inverse"></i>
-                            </span>
-                            Maynor Lopez
-                            <i class="fa fa-sign-out fa-2x" aria-hidden="true"></i>Logout
+                        <p class="navbar-text navbar-right">
+                            <button type="button" class="btn btn-link">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-flag fa-stack-1x fa-inverse"></i>
+                                </span>
+                                Maynor Lopez
+                            </button>
+                            <button type="button" class="btn btn-link">
+                                <i class="fa fa-sign-out fa-2x" aria-hidden="true"></i>Logout
+                            </button>
                         </p>
                     </div>
                 </nav>
@@ -149,16 +155,6 @@ and open the template in the editor.
                                                     </div>
                                                 </td>
                                             </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td></td>
-                                                <td></td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td></td>
-                                                <td></td>
-                                            </tr>
                                         </table>
                                     </div>
                                 </div>
@@ -173,8 +169,8 @@ and open the template in the editor.
                                 </div>
                                 <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
                                     <div class="panel-body">
-                                        <button id="paymentCash" onclick="showPaymentCash(this)" title="Pago en Efectivo" class="btn btn-warning btn-lg"> <i class="fa fa-money fa-3x"></i> <h3>Pago en Efectivo</h3></button>
-                                        <button id="paymentOnline" onclick="showPaymentOnline(this)" title="Pago en Linea" class="btn btn-warning btn-lg"> <i class="fa fa-cc-visa fa-3x"></i> <h3>Pago Online</h3></button>                                        
+                                        <div id="paymentCash" onclick="showPaymentCash(this)" title="Pago en Efectivo" class="btn btn-warning btn-lg"> <i class="fa fa-money fa-3x"></i> <h3>Pago en Efectivo</h3></div>
+                                        <div id="paymentOnline" onclick="showPaymentOnline(this)" title="Pago en Linea" class="btn btn-warning btn-lg"> <i class="fa fa-cc-visa fa-3x"></i> <h3>Pago Online</h3></div>                                        
                                     </div>
                                 </div>
                             </div>
@@ -188,13 +184,84 @@ and open the template in the editor.
                                 </div>
                                 <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
                                     <div class="panel-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                        <div class="col-md-6">
+                                            <strong class="col-md-2">Nombre:</strong>
+                                            <p id="nombreCliente" class="col-md-10"></p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <strong class="col-md-2">Direccion:</strong>
+                                            <p id="DireccionCliente" class="col-md-10"></p>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <table class="table table-bordered table-hover table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Nombre</th>
+                                                        <th>Precio (USD)</th>
+                                                        <th>Cantidad</th>
+                                                        <th>Subtotal (USD)</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Alitas con salsa atomica</td>
+                                                        <td>5.99</td>
+                                                        <td>1</td>
+                                                        <td>5.99</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Alitas con salsa hawaiana</td>
+                                                        <td>5.99</td>
+                                                        <td>2</td>
+                                                        <td>11.89</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Vegetales Extra</td>
+                                                        <td>1.99</td>
+                                                        <td>3</td>
+                                                        <td>5.97</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-success btn-block"><h4>Crear Pedido</h4></button>
                     </form>
+                    <!-- Modal -->
+                    <div class="modal fade" id="FormaDePago" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Datos de Pago</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <form method="POST">
+                                        <div class="radio-inline">
+                                            <label>
+                                                <input type="radio" name="Factura" value="FAC" />
+                                                Consumidor Final
+                                            </label>
+                                        </div>
+                                        <div class="radio-inline">
+                                            <label>
+                                                <input type="radio" name="Factura" value="CCF"> 
+                                                Credito Fiscal
+                                            </label>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                    <button type="button" class="btn btn-primary">Guardar Datos</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-1"></div>
             </div>
