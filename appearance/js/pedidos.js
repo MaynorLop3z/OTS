e99c557c44639043d4235e1e178e607734c17243
MaynorLop3z <lj09002@ues.edu.sv>
2016-06-10 16:32:06 -0600

    eliminacion de detalles en el pedido

--- appearance/js/pedidos.js
+++ appearance/js/pedidos.js
@@ -1,30 +1,34 @@
 var codigo;
 var indice = 0;
-function agregarItemMenu(item){
+function agregarItemMenu(item) {
     codigo = item.id;
     console.log(codigo);
-};
-function showPaymentCash(fila){
+}
+;
+function showPaymentCash(fila) {
     codigo = fila.id;
     console.log(codigo);
     console.log("Cash");
     $("#FormaDePago").modal('toggle');
-};
-function showPaymentOnline(fila){
+}
+;
+function showPaymentOnline(fila) {
     codigo = fila.id;
     console.log(codigo);
     $("#FormaDePago").modal('toggle');
-};
+}
+;
 
 
-function searchClient(event){
-  var tecla = event.keyCode || event.which;
+function searchClient(event) {
+    var tecla = event.keyCode || event.which;
     if (tecla === 9) {
         //event.preventDefault();
         console.log("Tecla Tab");
     }
-};
-$(".itemMenu").submit(function(event) {
+}
+;
+$(".itemMenu").submit(function (event) {
     event.preventDefault();
     var $form = $(this);
     var nameItem = $form.find(".itemName").html();
@@ -34,26 +38,26 @@ $(".itemMenu").submit(function(event) {
     var nivel = $form.find("select[name='nivelHot'] option:selected").text();
     var quantity = $form.find("input[name='Quantity']").val();
     var precio = $form.find(".itemPrice").html();
-    
-  var fila = '<tr id="itemDetail'+indice+'">';
-  fila += '<td class="Dscription">'+nameItem+'</td>';
+
+    var fila = '<tr id="itemDetail' + indice + '">';
+    fila += '<td class="Dscription">' + nameItem + '</td>';
     if (typeSalsa !== null) {
-        fila += '<td class="salsa'+typeSalsa+'">'+salsa+'</td>';
-    }else {
+        fila += '<td class="salsa' + typeSalsa + '">' + salsa + '</td>';
+    } else {
         fila += '<td class="salsa"></td>';
     }
-  fila += '<td class="picante'+nivelHot+'">'+nivel+'</td>';
-  fila += '<td class="cantidad'+quantity+'">'+quantity+'</td>';
-  fila += '<td class="Price">'+precio+'</td>';
-  fila +='<td class="Acciones"><button id="itemDEL' + indice + '" onclick="eliminarItem(this)" title="Eliminar Item" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> </button></td>';
-  fila +="</tr>";
-  $('#detailOrder').append(fila);
+    fila += '<td class="picante' + nivelHot + '">' + nivel + '</td>';
+    fila += '<td class="cantidad' + quantity + '">' + quantity + '</td>';
+    fila += '<td class="Price">' + precio + '</td>';
+    fila += '<td class="Acciones"><button id="itemDEL' + indice + '" onclick="eliminarItem(this)" title="Eliminar Item" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> </button></td>';
+    fila += "</tr>";
+    $('#detailOrder').append(fila);
     indice++;
 });
-function eliminarItem(fila){
+function eliminarItem(fila) {
     var codeDetail = fila.id.substring(7);
-   console.log(codeDetail);
-    
-    
-};
+    $('#detailOrder').find('#itemDetail' + codeDetail).fadeOut("slow");
+    $('#detailOrder').find('#itemDetail' + codeDetail).remove();
+}
+;
 
