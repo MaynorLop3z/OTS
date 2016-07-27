8546fec964042553d9592a3bfd833db7638dfa97
MaynorLop3z <lj09002@ues.edu.sv>
2016-07-27 17:12:28 -0600

    test print

--- application/controllers/CPrinter.php
+++ application/controllers/CPrinter.php
@@ -1,5 +1,11 @@
 <?php
 
+include 'WebClientPrint.php';
+
+use Neodynamic\SDK\Web\WebClientPrint;
+use Neodynamic\SDK\Web\Utils;
+//http://www.neodynamic.com/Products/Help/WebClientPrintPHP2.0/index.html
+//http://www.neodynamic.com/products/printing/raw-data/php/
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class CPrinter extends CI_Controller {
@@ -9,8 +15,8 @@ class CPrinter extends CI_Controller {
     }
 
     public function index() {
-        $data["test"]=$this->input->get('Id');
-        $this->load->view('Comanda',$data);
+        $data["test"] = $this->input->get('Id');
+        $this->load->view('Comanda', $data);
     }
 
 }
--- application/views/Comanda.php
+++ application/views/Comanda.php
@@ -2,15 +2,28 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Print Preview</title>
+        <script>
+            function createFile() {
+            var fso = new new ActiveXObject("Scripting.FileSystemObject");
+            var s = fso.OpenTextFile("C:\test.txt", 8,true,0);
+            s.writeline("HI");
+            s.writeline("Bye");
+            s.writeline("-----------------------------");
+            s.Close();
+            console.log("CREADO");
+            };
+            createFile();
+        </script>
     </head>
     <body>
         <?php
         echo '<pre>Item                                      Descripcion<br>';
-        echo 'Wings Dobles                              Salsa Aparte</pre>'.$test;
+        echo 'Wings Dobles                              Salsa Aparte</pre>' . $test;
         ?>
         <script>
-        window.print();
+            window.print();
+            
         </script>
     </body>
 </html>
